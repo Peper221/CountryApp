6813ae110c7d92671430678456d5094aa6f57ad4
Mario <jmario_vz@outlook.com>
2024-09-21 19:42:08 -0700

    agregado carga inicial para persistencia

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -1,20 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
 import { Country } from '../../interfaces/country';
+import { Region } from '../../interfaces/region.type';
+
 
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
   styles: ``
 })
-export class ByRegionPageComponent {
+export class ByRegionPageComponent implements OnInit {
 
   public countries: Country[] = [];
+  public regions: Region[] = ["Africa", "America", "Asia", "Europe", "Oceania"];
+  public selectedRegion?:Region;
+
 
   constructor( private countriesService: CountriesService){ }
+  ngOnInit(): void {
+     this.countries = this.countriesService.cacheStore.byRegion.countries;
+     this.selectedRegion = this.countriesService.cacheStore.byRegion.region;
+  }
 
-  searchRegion(term: string){
-    this.countriesService.searchRegion(term).subscribe(countries =>{
+  searchRegion(region: Region){
+    this.selectedRegion = region;
+    this.countriesService.searchRegion(region).subscribe(countries =>{
       this.countries = countries;
     });
   }
